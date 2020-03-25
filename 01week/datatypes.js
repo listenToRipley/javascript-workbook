8e1d7f7303abae3bcac3324f81fae9f3fa408f03
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-03-24 20:26:35 -0500

    in class work

---
+++ 01week/datatypes.js
@@ -0,0 +1,27 @@
+'use strict'
+
+//write a JS program to display current date and time
+
+let now = new Date();
+
+console.log('the current date and time ', now);
+const theNumber = 5;
+const theString = theNumber.toString()
+
+console.log('The type of theString varaible is ', (typeof theString))
+console.log('The ty[e of theNumber variable is ', (typeof theNumber))
+
+console.log('The string is ', theString)
+console.log('the number is ', theNumber)
+
+const theOtherString = '11'
+const theOtherNumber = parseInt(theOtherString, 10)
+console.log(theOtherNumber);
+console.log('theOtherNumber is a type of ', (typeof theOtherNumber))
+
+let num1 = 345
+let num2 = 876
+
+console.log(num1 + num2)
+
+
