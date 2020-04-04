7057fd55b750afd579a5056b1d3a9f01c517ddb5
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-04-04 11:26:15 -0500

    add more pseudo code

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -12,8 +12,7 @@ let board = [
   [' ', ' ', ' ']
 ];
 
-let playerTurn = 'X';
-let player2 = 'O'
+let playerTurn = //? should this be a function?
 
 function printBoard() {
   console.log('   0  1  2');
@@ -24,13 +23,17 @@ function printBoard() {
   console.log('2 ' + board[2].join(' | '));
 }
 
+//do we need to associate the board with this section? 
 //return true if any row wins
 function horizontalWin() {
   // Your code here
   //loops for each second array options
   for (let i = 0; i > 3; i++) {
     //let all of the rows match, by looping through the second position
+    //does that include all iterations of i?  
+      // shouldn't be const since the i changes, right? 
     let rows = (([0][i]) || ([1][i]) || ([2][i]))
+      //what will this return? This doesn't seem right. 
     if(rows === 'X' || rows === 'O') {
       console.log('Horizontal for the win!')
     }
@@ -42,7 +45,10 @@ function verticalWin() {
   // Your code here
   for (let i = 0; i > 3; i++) {
     //let all of the columns match, by looping through the first position
+    //does that include all iterations of i?
+      //shouldn't be const, since the i is changing, right? 
     let columns = (([i][0]) || ([i][1]) || ([i][2])) 
+    //what will this return? This doesn't seem right. 
      if(columns === 'X' || columns === 'O') {
       console.log('Vertical for win!')
     }
@@ -52,7 +58,9 @@ function verticalWin() {
 //return true if any column diagonal wins 
 function diagonalWin() {
   // Your code here
-  if (([0][0] && [1][1] && [2][2]) || ([0][2] && [1][1] && [2][0])) 
+  const diagonal = ([0][0] && [1][1] && [2][2]) || ([0][2] && [1][1] && [2][0])
+  //what will this return? This doesn't seem right. 
+  if (diagonal === 'X' || diagonal === 'O') 
   {
     console.log('Diagonal for the win!')
   }
@@ -63,7 +71,7 @@ function diagonalWin() {
 function checkForWin() {
   // Your code here
   if ((horizontalWin())||(verticalWin())||(diagonalWin())) {
-    console.log('The winner is'  // figure out how to place the winner X or O 
+    console.log('The winner is '  // figure out how to place the winner X or O 
     )
   }
 }
@@ -73,11 +81,23 @@ function ticTacToe(row, column) {
   // Your code here
 
   //set the value to positions on the board
+      //row refers to the the index of which first section you are retrieving and column is the index within the second array which you are setting it in?
+  row = board([0], [1], [2])//index 0, 1, 2 of the first array
+
+      // how do I create a shorter list? 
+      const column1 = board([0][0],[1][0],[2][0])
+      const column2 = board([0][1],[1][1],[2][1])
+      const column3 = board([0][2],[1][2],[2][2])
+  column = (column1, column2, column3) //index 0, 1, 2 of the second array
+  
+  printBoard(row, columns)
   //assign player 1 = X
   //assign player 2 = O
+      // should they be able to choose? 
   //place player 1
     //limit play to one turn
   //check win conditions 
+    // don't need to check win conditions if there has been less than 3 placements 
     //if they did statement for win
   //place player 2
     //limit play to one turn
