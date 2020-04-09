d0eb8f0bb3a658cc196e5efcb00b5d9e99fa285a
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-04-08 21:08:46 -0500

    got my code to display in the console, but not how to associate the pigLatin function with the new word

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const assert = require('assert');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const assert = require('assert');
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
 
 const pigLatin = (word)  => {
@@ -35,45 +35,79 @@ else if (!vowel){
   
 }
 
-const getPrompt = () => {
-  rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
-    getPrompt();
-  });
-}
+// const getPrompt = () => {
+//   rl.question('word ', (answer) => {
+//     console.log( pigLatin(answer) );
+//     getPrompt();
+//   });
+// }
 
+
+//nothing will happen unless the button is clicked first 
+let translateButton = document.getElementById('translate')
+translateButton.addEventListener('click', function(){
+  console.log('this is the button you click to translate ', translateButton)
+
+//pulling the text field into my JS
+let wordInput = document.getElementById('funPrompt')
+console.log('here is the input field ', wordInput)
+//making the content on the text within the field actionable
+
+//make the input field equal to the word in the pigLatin function
+
+  //IMPORTANT NOTE!
+    //this would be the word before it is translated! 
+let newWord = wordInput.value
+console.log('the phase/word you are translating to PIG LATIN IS: ', newWord)
+
+//set the input field to blank after word is entered
+wordInput.value = ''
+
+//for each word entered, we want to create a new span
+let spanDisplayResult = document.createElement('span')
+spanDisplayResult.innerText = pigLatin(newWord)
+                  //we want the output create to be the pigLatin, not the word in its current state
+
+//create space with the div where I was the result to display
+let mainDisplay = document.querySelector('#mainOutput')
+  console.log('this is the mainDisplay where my result will go ',mainDisplay)
+mainDisplay.appendChild(spanDisplayResult)
+//place text inside of the input field
+
+  // perform the function in the pig latin
+//create an output field where the text displays 
+
+//create location for the new word to go into
 let results = document.getElementById('output')
-results.innerHTML = getPrompt()
+// results.innerHTML = getPrompt()
+})
 
 // Tests
-gas
-if (typeof describe === 'function') {
+// gas
+// if (typeof describe === 'function') {
 
-  describe('#pigLatin()', () => {
-    it('should translate a simple word', () => {
-      assert.equal(pigLatin('car'), 'arcay');
-      assert.equal(pigLatin('dog'), 'ogday');
-    });
-    it('should translate a complex word', () => {
-      assert.equal(pigLatin('create'), 'eatecray');
-      assert.equal(pigLatin('valley'), 'alleyvay');
-    });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin('egg'), 'eggyay');
-      assert.equal(pigLatin('emission'), 'emissionyay');
-    });
-    it('should lowercase and trim word before translation', () => {
-      assert.equal(pigLatin('HeLlO '), 'ellohay');
-      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
-    });
-  });
-} else {
+//   describe('#pigLatin()', () => {
+//     it('should translate a simple word', () => {
+//       assert.equal(pigLatin('car'), 'arcay');
+//       assert.equal(pigLatin('dog'), 'ogday');
+//     });
+//     it('should translate a complex word', () => {
+//       assert.equal(pigLatin('create'), 'eatecray');
+//       assert.equal(pigLatin('valley'), 'alleyvay');
+//     });
+//     it('should attach "yay" if word begins with vowel', () => {
+//       assert.equal(pigLatin('egg'), 'eggyay');
+//       assert.equal(pigLatin('emission'), 'emissionyay');
+//     });
+//     it('should lowercase and trim word before translation', () => {
+//       assert.equal(pigLatin('HeLlO '), 'ellohay');
+//       assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+//     });
+//   });
+// } else {
 
-  getPrompt();
+//   getPrompt();
 
-}
-
-
-
+// }
 
 
