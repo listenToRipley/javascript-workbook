0469e1b5dc95cf0789c6a88415539e464567293e
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-04-08 19:20:54 -0500

    added css to the to do

--- 02week/todo.js
+++ 02week/todo.js
@@ -3,29 +3,43 @@ console.log('This my js page')
 //add button clicked, 
   //append the list to add the new text to bottom of the list
 
-let addButton = document.getElementById('add')
+let addButton = document.getElementById('addButton')
 
 addButton.addEventListener('click', function() {
-  console.log("my delete button ", addButton)
+  // console.log("my add button ", addButton)
 
-//   the new content should look like this
-//   <li>
-//   <span class="done">inner text here</span> 
-//     <button class="delete">delete</button>
-// </li>
+//sets the content
+let inputElement = document.getElementById('inputText');
+let todoText = inputElement.value
 
-let toDoText = document.getElementById('newContent').nodeValue;
+//makes sure the text goes back to blank after the text is entered
+inputElement.value = '';
+
+//create my list item
 let li =  document.createElement('li')
-let span =   document.createElement('span').innerText = toDoText
 
+//creates a span item 
+let span = document.createElement('span')
+//appends the inner text to the list
+  span.innerText = todoText
+
+//creates a delete button
 let deleteButton = document.createElement('button')
+
+//updates the text on the delete button
   deleteButton.innerText = 'delete'
+
+//adds a class for the delete button, makes it so you can edit it in css if you want to
   deleteButton.classList.add('delete')
 
-let ul = document.getElementsByTagName('ul')
+//adds the list item to the bottom of the of your ul 
+let ul = document.querySelector('ul')
 ul.appendChild(li)
+
+// adds the span 
 li.appendChild(span)
 setUpSpanEvent(span)
+//and delete button as the children of the new li 
 li.appendChild(deleteButton)
 setupDeleteEvent(deleteButton)
 })
@@ -33,34 +47,33 @@ setupDeleteEvent(deleteButton)
 //delete button clicked, 
   //remove the individual item associated with the line item, the parent list item 
 let allDeletes = document.querySelectorAll('.delete')
-for (let i = 0; i < allDeletes.length; i++) {
+for(let i=0; i<allDeletes.length; i++) {
   let deleteButton = allDeletes[i]
-  function setupDeleteEvent(deleteButton) {
-    deleteButton.addEventListener('click', function() {
-      console.log(`Delete got clicked, it's parent node is, `, deleteButton.parentElement)
-      let parentLi = deleteButton.parentElement
-      let parentUl = parentLi.parentElement
-      parentUl.removeChild(parentLi)
-    })
-  } 
+  setupDeleteEvent(deleteButton);
 }
 
-//when the span is clicked, 
-  //class "done" added to it 
-  //create a strike through effect 
+function setupDeleteEvent(deleteButton) {
+  deleteButton.addEventListener('click', function() {
+    console.log('Delete got clicked! parent li is, ', deleteButton.parentElement)
+    let parentLi = deleteButton.parentElement
+    parentLi,remove()
+  })
+}
 
-  let allSpans = document.querySelectorAll('span')
-  console.log(allSpans, 'is here');
+//when span / to do item, get clicked
+//the done class should be added to it 
+let allSpans = document.querySelectorAll('span')
+  // console.log(allSpans, 'is here');
 
 for(let i = 0; i < allSpans.length; i++) {
   let span = allSpans[i];
-  // console.log('The span is here', span)
+  setUpSpanEvent(span)
 }
 
+//also want to add the click event to the span
 function setUpSpanEvent(span) {
-
   span.addEventListener('click', function() {
-    console.log('now the span is here', span);
-    span.classList.add('done');
+    console.log('now the line is here', span);
+    span.classList.toggle('done');
   })
 }
