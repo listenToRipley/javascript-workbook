c6e1720bca6dccbe4704c782440bb9bb47d769bc
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-04-12 21:29:39 -0500

    started the general idea of the code.

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -7,6 +7,7 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+//remember, these are your towers/ your board
 let stacks = {
   a: [4, 3, 2, 1],
   b: [],
@@ -19,23 +20,78 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
+//make sure you're using shift and unshift since you always want it to be the last one 
+
 function movePiece() {
   // Your code here
+  //take the last item off of stack and move it
 
+//this will always be your starting state 
+let stackA = stacks.a
+let stackB = stack.b
+let stackC = stack.c
+
+//establish the index of your array in each object
+let lastStackA = stackA[stackA.length-1]
+let lastStackB = stackB[stackB.length-1]
+let lastStackC = stackC[stackC.length-1]
+
+//all the move from s 
+let removeFromA = stackA.pop(lastStackA)
+let removeFromB = stackB.pop(lastStackB)
+let removeFromC = stackC.pop(lastStackC)
+
+//move combinations 
+
+//move from stack a to stack c
+// let aToC = 
+stackC.push(removeFromA)
+
+//move from a stack to stack b
+// let aToB = 
+stackB.push(removeFromA)
+
+//move from stack b to stack a 
+// let bToA = 
+stackB.push(removeFromA)
+
+//move from stack b to stack c
+//let bToC = 
+stackB.push(removeFromC)
+
+//move from stack c to stack a 
+//let cToA = 
+stackC.push(removeFromA)
+
+//move from stack c to stack b 
+//let cToB = 
+stackC.push(removeFromB)
 }
 
-function isLegal() {
+function isLegal(startStack, endStack) {
   // Your code here
+  // you cannot occupy the space of a smaller number  
+    //nor can you move to the same space 
+    //we need to make sure to compare the value of your current move to the last array value already there.
+  if((endStack < startStack)||(startStack === endStack)) {
+    return false
+  } 
 
 }
 
 function checkForWin() {
   // Your code here
+  //you move all your values from stack a to stack c and the order is the same as the beginning 
+if(stackC === [4, 3, 2, 1]) {
+  return true 
+}
 
 }
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
+
+//we want to startStack to equal where you are moving from to where you are moving to. The value should always be the one on top.
 
 }
 
