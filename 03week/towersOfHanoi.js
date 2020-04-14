26299183d4434e2776781a17f59ca6bf4fd9565d
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-04-13 21:29:32 -0500

    try working out the code

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -22,16 +22,17 @@ function printStacks() {
 
 //make sure you're using shift and unshift since you always want it to be the last one 
 
+//this will always be your starting state 
+let stackA = stacks.a
+let stackB = stacks.b
+let stackC = stacks.c
+
 function movePiece() {
   // Your code here
   //take the last item off of stack and move it
 
-//this will always be your starting state 
-let stackA = stacks.a
-let stackB = stack.b
-let stackC = stack.c
-
 //establish the index of your array in each object
+  //should this be created as it's own object? 
 let lastStackA = stackA[stackA.length-1]
 let lastStackB = stackB[stackB.length-1]
 let lastStackC = stackC[stackC.length-1]
@@ -41,40 +42,47 @@ let removeFromA = stackA.pop(lastStackA)
 let removeFromB = stackB.pop(lastStackB)
 let removeFromC = stackC.pop(lastStackC)
 
-//move combinations 
+    //move combinations 
+    //we only want to push if the item is called 
 
 //move from stack a to stack c
-// let aToC = 
-stackC.push(removeFromA)
-
-//move from a stack to stack b
-// let aToB = 
-stackB.push(removeFromA)
-
-//move from stack b to stack a 
-// let bToA = 
-stackB.push(removeFromA)
-
-//move from stack b to stack c
-//let bToC = 
-stackB.push(removeFromC)
-
-//move from stack c to stack a 
-//let cToA = 
-stackC.push(removeFromA)
-
-//move from stack c to stack b 
-//let cToB = 
-stackC.push(removeFromB)
+if(((startStack === stackA) && (endStack === stackC))) {
+  stackC.push(removeFromA)
 }
 
-function isLegal(startStack, endStack) {
+//move from a stack to stack b
+else if (((startStack === stackA) && (endStack === stackB))) {
+  stackB.push(removeFromA)
+} 
+//move from stack b to stack a 
+else if (((startStack === stackB) && (endStack === stackA))) {
+  stackB.push(removeFromA)
+} 
+
+//move from stack b to stack c
+else if (((startStack === stackB) && (endStack === stackC))) {
+  stackB.push(removeFromC)
+} 
+
+//move from stack c to stack a 
+else if (((startStack === stackC) && (endStack === stackA))) {
+  stackC.push(removeFromA)
+} 
+
+//move from stack c to stack b 
+else if (((startStack === stackC) && (endStack === stackB))) {
+  stackC.push(removeFromB)
+} 
+
+}
+
+function isLegal() {
   // Your code here
-  // you cannot occupy the space of a smaller number  
-    //nor can you move to the same space 
+  // if the last number placed in the space is smaller then the number you are currently trying to place, then you cannot place that new number
+    //you cannot move to the same space you started at
     //we need to make sure to compare the value of your current move to the last array value already there.
   if((endStack < startStack)||(startStack === endStack)) {
-    return false
+    return true
   } 
 
 }
@@ -82,6 +90,7 @@ function isLegal(startStack, endStack) {
 function checkForWin() {
   // Your code here
   //you move all your values from stack a to stack c and the order is the same as the beginning 
+    //should I just a for in loop here to verify it? 
 if(stackC === [4, 3, 2, 1]) {
   return true 
 }
@@ -90,8 +99,25 @@ if(stackC === [4, 3, 2, 1]) {
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
+    //we want to startStack to equal where you are moving from 
+   //and the endStack  equal to where you are moving to. 
 
-//we want to startStack to equal where you are moving from to where you are moving to. The value should always be the one on top.
+  //first verify you move if legal
+  if (isLegal(startStack, endStack) === false) {
+    //then you can move a piece 
+    movePiece(startStack, endStack) 
+    //then you want to see if you win
+    if (checkForWin() === true) {
+      // if you win the game is over
+      console.log('You win!')
+    } else {
+      //else you are going to need to keep playing
+      movePiece(startStack, endStack)
+    }
+    //see of the move is illegal, then you won't continue!
+  } else {
+    console.log('try again')
+  }
 
 }
 
