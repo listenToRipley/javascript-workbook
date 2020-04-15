256c99fdc1a9bbe5baf0913b6d20e2d34332ed86
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-04-15 15:54:01 -0500

    added helper functions within functions so code is not repeated

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -31,75 +31,47 @@ function movePiece() {
 
 
 //establish the index of your array in each object
-  //should this be created as it's own object? 
-let lastStackA = stackA[stackA.length-1]
-let lastStackB = stackB[stackB.length-1]
-let lastStackC = stackC[stackC.length-1]
+// let lastStackA = stackA[stackA.length-1]
+// let lastStackB = stackB[stackB.length-1]
+// let lastStackC = stackC[stackC.length-1]
+  //or we could create another helper function to make this a lot shorter 
+                          //should this be starter stack maybe?
+  let lastInStackIndex = (startStack) => {
+    return startStack.length-1
+  }
 
   //take the last item off of stack and move it
-let removeFromA = stackA.pop(lastStackA)
-let removeFromB = stackB.pop(lastStackB)
-let removeFromC = stackC.pop(lastStackC)
-
-//this should be placed here? Or in the last function? 
-//all start should pop the last item on the index last 
-let startStack = (removeFromA || removeFromB || removeFromC)
+// let removeFromA = stackA.pop(lastStackA)
+// let removeFromB = stackB.pop(lastStackB)
+// let removeFromC = stackC.pop(lastStackC)
+  // or we could create another function to go through each
+                    // should this still share the same variable name? 
+  let removeFrom = (startStack) => {
+    return startStack.pop(lastInStackIndex)
+  }
 
 //all ends should push the pop item on the next index,
-  //this is not current right, it would just continuously pop 
-let endStack = (removeFromA || removeFromB || removeFromC)
+    //need to push the popped it here, which is what we were doing below
+  let moveTo = (endStack) => {
+    return endStack.push(removeFrom(startStack))
+  }
 
-    //move combinations 
-    //we only want to push if the item is called 
-//we want to first make sure this is a valid move 
-  //and win condition has not yet been met
-  //is this considered hoisting? should this be placed in the last function?
-if ((isLegal === true) && (checkForWin === false)) {
-//move from stack a to stack c
-if(((startStack === stackA) && (endStack === stackC))) {
-  stackC.push(removeFromA)
-}
-
-//move from a stack to stack b
-else if (((startStack === stackA) && (endStack === stackB))) {
-  stackB.push(removeFromA)
-} 
-//move from stack b to stack a 
-else if (((startStack === stackB) && (endStack === stackA))) {
-  stackB.push(removeFromA)
-} 
-
-//move from stack b to stack c
-else if (((startStack === stackB) && (endStack === stackC))) {
-  stackB.push(removeFromC)
-} 
-
-//move from stack c to stack a 
-else if (((startStack === stackC) && (endStack === stackA))) {
-  stackC.push(removeFromA)
-} 
-
-//move from stack c to stack b 
-else if (((startStack === stackC) && (endStack === stackB))) {
-  stackC.push(removeFromB)
-} 
-} else {
-  return false 
-  console.log('try again! not valid move!')
-}
+  //LAST THREE FUNCTIONS SHOULD BE DAISY CHAINED
 
 }
 
 function isLegal() {
+
   // Your code here
     // the item being pop is greater then the last item push to that location
-  if((endStack < startStack)
+      //need to asses the last item in the index of your current move  
+      let lastValueInStack = (stack) => {
+        return stack.length-1
+      }
+  if((endStack[lastValueInStack(endStack)] < startStack[lastValueInStack(startStack)])
     // of the starting position happens to be the same as the end position 
       ||(startStack === endStack)) {
-    return true
-  } else {
-    return false 
-    console.log('try again!')
+    return false
   }
 
 }
@@ -116,9 +88,13 @@ if(stackC === [4, 3, 2, 1]) {
 function towersOfHanoi(startStack, endStack) {
   // Your code here
 
-  //all other functions have been built into the other functions 
+  //this isn't quite right, we do call these into the move piece, but not right
+    if (checkForWin === true) {
+      return 'you win'
+    } else {
     movePiece(startStack, endStack) 
-
+      if (move)
+    }
 }
 
 function getPrompt() {
