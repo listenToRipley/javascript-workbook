fb125cc3da96c1fca4854e220ed545a2fa54b04c
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-04-16 08:40:51 -0500

    last merge request for tic tac toe was wrong

--- 02week/ticTacToe.js
+++ 02week/ticTacToe.js
@@ -14,8 +14,6 @@ let board = [
 
 // tic-tac-toe
 let playerTurn = 'X';
-// 
-let playerTurn = //? should this be a function?
 
 function printBoard() {
   console.log('   0  1  2');
@@ -26,85 +24,70 @@ function printBoard() {
   console.log('2 ' + board[2].join(' | '));
 }
 
-//do we need to associate the board with this section? 
 //return true if any row wins
 function horizontalWin() {
   // Your code here
-  //loops for each second array options
-  for (let i = 0; i > 3; i++) {
-    //let all of the rows match, by looping through the second position
-    //does that include all iterations of i?  
-      // shouldn't be const since the i changes, right? 
-    let rows = (([0][i]) || ([1][i]) || ([2][i]))
-      //what will this return? This doesn't seem right. 
-    if(rows === 'X' || rows === 'O') {
-      console.log('Horizontal for the win!')
-    }
-  }
-}
+  //loops through each option for the second array
+  for (let i = 0; i >= 2; i++) {
 
+    //checked is any of the whole rows returns with the same value 
+    if (((board[0][i]) || (board[1][i]) || (board[2][i])) === playerTurn) 
+    { 
+        console.log('row win') 
+      }
+    }
+    return true
+  }
+
+
+//this is a helper function for checkForWin()
 //return true if any column wins
 function verticalWin() {
-  // Your code here
-  for (let i = 0; i > 3; i++) {
-    //let all of the columns match, by looping through the first position
-    //does that include all iterations of i?
-      //shouldn't be const, since the i is changing, right? 
-    let columns = (([i][0]) || ([i][1]) || ([i][2])) 
-    //what will this return? This doesn't seem right. 
-     if(columns === 'X' || columns === 'O') {
-      console.log('Vertical for win!')
+  // Loop first array options
+  for (let i = 0; i >= 2; i++) {
+
+    //checks if the whole column shares the same value 
+     if(((board[i][0]) || (board[i][1]) || (board[i][2])) === playerTurn)  {
+      console.log('column win')
     }
   }
+  return true
 }
 
+//this is a helper function for checkForWin()
 //return true if any column diagonal wins 
 function diagonalWin() {
-  // Your code here
-  const diagonal = ([0][0] && [1][1] && [2][2]) || ([0][2] && [1][1] && [2][0])
-  //what will this return? This doesn't seem right. 
-  if (diagonal === 'X' || diagonal === 'O') 
+  
+  //looks at the whole of either left diagonal or right diagonal 
+  if ((board[0][0] && board[1][1] && board[2][2]) || (board[0][2] && board[1][1] && board[2][0]) === playerTurn) 
   {
-    console.log('Diagonal for the win!')
+    return true
   }
-
 }
 
-//which against your win functions 
+//which against your win functions, this function must come before the player change 
 function checkForWin() {
   // Your code here
   if ((horizontalWin())||(verticalWin())||(diagonalWin())) {
-    console.log('The winner is '  // figure out how to place the winner X or O 
-    )
+    return true 
+  } else {
+    return false 
   }
 }
 
 
 function ticTacToe(row, column) {
   // Your code here
+  board[row][column] = playerTurn
 
-  //set the value to positions on the board
-      //row refers to the the index of which first section you are retrieving and column is the index within the second array which you are setting it in?
-  row = board([0], [1], [2])//index 0, 1, 2 of the first array
+  //this function must be called before you switch players or you would only be assessing based on one player 
+  checkForWin(playerTurn)
 
-      // how do I create a shorter list? 
-      const column1 = board([0][0],[1][0],[2][0])
-      const column2 = board([0][1],[1][1],[2][1])
-      const column3 = board([0][2],[1][2],[2][2])
-  column = (column1, column2, column3) //index 0, 1, 2 of the second array
-  
-  printBoard(row, columns)
-  //assign player 1 = X
-  //assign player 2 = O
-      // should they be able to choose? 
-  //place player 1
-    //limit play to one turn
-  //check win conditions 
-    // don't need to check win conditions if there has been less than 3 placements 
-    //if they did statement for win
-  //place player 2
-    //limit play to one turn
-  //check win conditions 
+  //if statement here for ties! if checkForWin returns false and all squares are populated 
+    
+  //alternates between X and O
+  playerTurn = (playerTurn === 'X') ? 'O' : 'X'
+
 }
 
 function getPrompt() {
