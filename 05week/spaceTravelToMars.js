9ed91ced049ae80bfc33b47f0b94881fb9c83079
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-04-21 21:10:41 -0500

    more notes

--- 05week/spaceTravelToMars.js
+++ 05week/spaceTravelToMars.js
@@ -16,12 +16,62 @@ let jobTypes = {
 //crew member
   //constructor should take in as input: name, their job, their special skill 
 
+class CrewMember {
+  constructor(name, job, skill) {
+    this.name = name;
+    this.job = job;
+    this.skill = skill;
+
+    //we need to assign this information back the crew member 
+    this.postedToShip = null;
+      //unlike ship, we're only going to have one ship assign to one individual 
+
+    //this method should add THIS crew member to the ship being passed in.
+    //NOTE: an entire ship instance is passed in, not just a name
+    //NOTE: the entire crew number should be added to the ship's array of crew 
+    // enterShip(someShip){
+        //pass the ship name to the individual 
+      //}
+    // }
+
+  }
+}
+
 //ship 
   //constructor should take in as input:name, type, ability
     //can have crew members 
     //contain a method: missionStatement()
         //if there a crew member that can activate it  == loop through crew to make sure jobTypes matches ship ability, it should return ability
           //else return 'can't perform a mission yet' 
+
+class Ship {
+  constructor(shipName, type, ability) {
+
+    this.shipName = shipName;
+    this.shipType = shipType;
+    this.ability = ability
+
+    //need to have an empty array for crew members
+    this.crew = [
+        //we need to push the whole class on the member in here. 
+    ]
+
+    //we need to assign a crew member to a ship
+    //update the crew members in the ship 
+
+    this.crew.postedToShip = shipName
+    this.shipName.crew = crew
+    //this method should return the ship's ability if there is a crew member who job matches that require abilities aka "ship type"
+    // missionStatement() {
+        // we need to compare the jobTypes to the type of ship
+    //   }
+    // }
+
+    // description () {
+    //   //provide object literals for the details on what should be getting passed in  
+    // }
+  }
+}
 
 //tests
 if (typeof describe === 'function'){
