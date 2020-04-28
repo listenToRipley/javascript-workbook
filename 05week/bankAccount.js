e2d447e3b8a140043b1cc6e84ac4aaf4da3fd101
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-04-27 21:40:36 -0500

    modified transactions to a super

--- 05week/bankAccount.js
+++ 05week/bankAccount.js
@@ -5,40 +5,46 @@ class BankAccount {
     this.accountNumber = accountNumber;
     this.owner = owner;
     this.transaction = [0];
+    this.history = []
+    
   }
 
   balance() {
     //shows the total of the transaction array
       //can never reach less than 0
-    return this.transaction[0]
+    return this.transaction.reduce(acc, value => acc + value)
   }
 
-  deposits(amt) {
+  deposits(amount) {
     //shows are refunds or deposits by account holder to the account
       // will always be a positive number 
-     this.transaction = amt + this.transaction[0]
+    this.transaction.push(amount)
   }
 
-  charge(payee, amt) {
-    //shows are charges to the account 
-      //will always be a negative number 
-        //cannot subtract from a balance of zero
-      this.transaction = this.transaction[0] + amt 
-
-    if (this.transaction[0] <= 0 || this.transaction < amt) {
+  charge(payee, amount) {
+  //shows are charges to the account 
+    //prevent a transaction being posted to the card if
+      //the balance is zero or the amount is greater than balance 
+    if (this.balance() <= 0 || this.balance() < amount) {
       console.log('insufficient funds')
+    } else {
+    //will always be a negative number
+      this.transaction.push(-Math.abs(amount))
     }
   }
 }
 
-class Transaction {
-  constructor(date, amount, payee, vendorName) {
+//this is where a transaction is created
+class Transaction extends BankAccount {
+  super(date, amount, payee) {
     this.date = date;
     this.amount = amount; 
     this.payee = payee;
-    this.vendorName = vendorName
   } 
-    history() {
+    //we want to store all transactions associated with the card
+    history(aTransaction) {
       //shows the complete details of the transaction 
-    }
+      //this will be a complete history of all transactions 
+    this.history.push(aTransaction)
+}
 }
