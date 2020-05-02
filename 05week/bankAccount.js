137ccb385eac3c47e2db70776566c013bb15154a
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-05-01 19:02:59 -0500

    got the transactions to push to my array, the balance is not working quite right.

--- 05week/bankAccount.js
+++ 05week/bankAccount.js
@@ -7,49 +7,41 @@ class BankAccount {
       //this is supposed to be the transaction history 
     this.transactions = [];
   }
+  
+  //return the sum of all amount, subtracting and adding as needed 
 
-  //change all of these since we need to access 
-
+  //~~isn't returning the correct values 
   balance() {
     let balance = 0
-    console.log(`before`,balance)
+    // console.log(`before`,balance)
     //reduce down all amounts 
+    if (this.transactions.length === 0) {
+      return 'this is a new account, no deposits have been mase '
+    } else {
     this.transactions.reduce((acc, value) => 
-      {
+     {
       //  console.log(acc, value)  
-       return balance = acc + value.amount
-      })
-      console.log(`after`,balance)
+     return balance = acc + value.amount
+    })
+    }
+}
 
-  //if the balance is at zero
-  if (balance = 0) {
-    return 'this is a new account, a deposit has not yet been made'
-  } else {
-    return balance
-  }
- }
-
-
- //~~something not working here 
   deposits(amount) {
     // console.log('can we see inside the deposit?')
     //should always be the account number doing the deposit 
     let payee = this.owner
       //if we are just pushing in the account, would there be a way to default the payee information to just say deposit? 
     this.transactions.push(new Transaction(amount, payee))
-
   }
 
   charge(aTransaction) {
-
     //prevent a transaction being posted to the card if
       //the balance is zero or the amount is greater than balance 
-    if (this.balance() <= 0 || this.balance() < amount || this.balance() === 'this is a new account, a deposit has not yet been made') {
+    if (this.balance() <= 0 || this.balance() < this.amount || this.balance() === 'this is a new account, a deposit has not yet been made') {
       return 'insufficient funds'
     } else {
     return this.transactions.push(aTransaction)
   }
-
 }
 }
 
@@ -65,13 +57,18 @@ class Transaction {
 }
 
 let myAccount = new BankAccount(123244, 'Natalie Kendrick')
-console.log(myAccount)
+// console.log(myAccount)
 let yourAccount = new BankAccount(234236, 'Willie Nelson')
-console.log(yourAccount)
+// console.log(yourAccount)
 
-let silverSea = new Transaction(313, 'Silver Sea')
-console.log(silverSea)
+let silverSea = new Transaction(-313, 'Silver Sea')
+let pTerrys = new Transaction(-24.53, 'P.Terry\'s')
+// console.log(silverSea)
 myAccount.deposits(1231)
 myAccount.balance()
+// console.log(myAccount)
+myAccount.charge(silverSea)
+myAccount.charge(pTerrys)
 console.log(myAccount)
-console.log(`charge: `,myAccount.charge(silverSea))
+myAccount.balance()
+
