0250bb95df3f6da7a11d9a679faf4532d8a13fb9
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-05-09 17:34:33 -0500

    my node tree is wrong, info on top now

--- 07week/addressBook.js
+++ 07week/addressBook.js
@@ -24,13 +24,10 @@ let apiPeople = function() {
 }
 
 //should print ten lines of people
-let findPeople = function(person) {
-  // console.log('find the people = ', person)
-
+let findPeople = function(person, index) {
+  console.log('find the people = ', person)
+  console.log('index', index)
   //create my HTML set up 
-  let body = document.getElementsByTagName('body')
-  console.log(body)
-  //create your elements that you will be working with 
   //CREATION OF ELEMENTS
     //main body of my content, this is the master element 
   let mainBody = document.createElement('main')
@@ -42,30 +39,29 @@ let findPeople = function(person) {
   let mainUl = document.createElement('ul')
   // console.log('my main ul where the name and picture will first appear', mainUl )
   //create a separate UL for all the extra information 
-  let hiddenUL = document.createElement('ul')
-  // console.log('my second ul, where the additional information on each person will live', hiddenUL)
 
-    //class names in order 
-    div.setAttribute('id', 'people') //book
-    mainUl.setAttribute('id','person')
-    hiddenUL.setAttribute('id','extraInfo')
+  //class names in order 
+  div.setAttribute('id', 'people') //book
+  mainUl.setAttribute('id','person')
     
-
   //make sure my parent elements know how they are nested 
   mainBody.appendChild(div)
   div.appendChild(mainUl)
-  mainUl.appendChild(hiddenUL)
   // console.log('everything coming through?',div) yes
 
   //query selectors 
   let book = document.getElementById('people')
-    // console.log('this is all the information',book)
+    console.log('this is all the information',book)
+
+
+let indexUsed = []
+  //we want the UL to be created per person
+  //using the index, determine if you have already created a UL for the person
+  //every time you do create a new UL, add the index number to your indexUsedArray
+  //if not, create UL 
+  if (indexUsed.includes(index) === false) {
   let individual = document.getElementById('person')
     // console.log('this is each person coming in from the api',individual)
-  let hidInfo = document.getElementById('extraInfo')
-  // console.log('this is the additional information on each person', hidInfo)
-  
-  // console.log('this is my button', btn)
 
   //these are my created elements within the mainUL
   let profilePic = document.createElement('img')
@@ -94,39 +90,52 @@ let findPeople = function(person) {
   individual.appendChild(firstNames)
   individual.appendChild(lastNames)
 
-  //this should be the content on your hidInfo UL list 
-  let dob = document.createElement('li')
-  let city = document.createElement('li')
-  let state = document.createElement('li')
-  let country = document.createElement('li')
-  let phone = document.createElement('li')
-  let email = document.createElement('li')
-
-  //add the information that will be contained in the extraInfo section
-  dob.innerText = person.dob.date
-  city.innerText = person.location.city
-  state.innerText = person.location.state
-  country.innerText = person.location.country
-  phone.innerText = person.phone
-  email.innerText = person.email
-
-  console.log('now this should have all the info per person',hidInfo)
 
   infoButton.setAttribute('id','infoBtn')
   //the information should not display until the button is clicked. 
     //when it is clicked, it should display directly under the individual 
   infoButton.addEventListener('click', function() {
     console.log('see this info button get clicked?', infoButton)
-      //add those elements to the extraInfo section
+    //don't want this information created until after the button
+    let hiddenUL = document.createElement('ul')
+    // console.log('my second ul, where the additional information on each person will live'hiddenUL)
+    hiddenUL.setAttribute('id','extraInfo')
+    let hidInfo = document.getElementById('extraInfo')
+    // console.log('this is the additional information on each person', hidInfo)
+    individual.appendChild(hiddenUL)
+
+    //this should be the content on your hidInfo UL list 
+    let dob = document.createElement('li')
+    let city = document.createElement('li')
+    let state = document.createElement('li')
+    let country = document.createElement('li')
+    let phone = document.createElement('li')
+    let email = document.createElement('li')
+
+    //add the information that will be contained in the extraInfo section
+    dob.innerText = person.dob.date
+    city.innerText = person.location.city
+    state.innerText = person.location.state
+    country.innerText = person.location.country
+    phone.innerText = person.phone
+    email.innerText = person.email
+
+    // console.log('now this should have all the info per person',hidInfo)
+    
+      //add those elements information 
     hidInfo.appendChild(dob)
     hidInfo.appendChild(city)
     hidInfo.appendChild(state)
     hidInfo.appendChild(country)
     hidInfo.appendChild(phone)
     hidInfo.appendChild(email)
-    individual.appendChild(hidInfo)
-    // console.log(one)
-    console.log(hidInfo)
+      console.log(hidInfo)
+
+    mainUl.appendChild(hiddenUL)
   })
+
+  //add this index to your indexUsed array so you don't make it again
+  indexUsed.push(index)
+} 
 }
 
