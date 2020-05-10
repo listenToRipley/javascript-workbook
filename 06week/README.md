3b512bc5c9066b9e7829fa7f55b3c7b9fe88dacf
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-05-10 17:19:37 -0500

    wrote out more code plan, got new object with object.assign

--- 06week/README.md
+++ 06week/README.md
@@ -27,6 +27,73 @@ Sort those individuals into 1 of 2 teams
   Players can move from one team to another, but they should only appear on one list at a time. 
     If a play is moved from one team to another, the team information should be replaced with the team they moved to. 
 
+Important questions to ask:
+**EVEYTIME 
+  What order does this need to occur in?
+  Is this action dependent on something else occur first?
+  Where will my content live in the html?
+**What code will be repeated?
+     There seems to be a lot of creation of li's and buttons and removal from lists. 
+
+**What is the most unique attribute of each person? 
+Their ID
+
+**What triggers these events? 
+  Most of these events are based off on button clicks, so the functions for each should be associated with the related button. 
+  ~~these two button are already created for use, so we do not need to create them again 
+List Of People = listPeopleChoices()
+Make a person into a player = makePlayer()
+
+~~ buttons I need to create
+@ Make person part of the blue team = blue()
+@ Make person part of the red team = team()
+@ Change teams = exchange()
+#would like
+@ Edit class player, update their abilities = edit()
+
+**What events are associated with each of these events? 
+listPeopleChoices() 
+  Lives in - 'people'
+    maps a new object from arrayOfPeople and create a list 
+    creates an li and button
+    creates a click event for the new button 
+      appends the DOM to include the player's name, their skill set and the button the will allow them to become a player
+~~
+  makePlayer()
+  Lives in - 'player'
+    get the id of player
+    using the ID, create a copy of that person in   listOfPlayers[] 
+    remove that li from the the list of people
+    add the class of Player to that person 
+    add a buttons of blue() & red()
+      this function should be triggered inside of this element 
+    update html content
+
+  blue() & red()
+    Lives in - 'blue' & 'red' respectively 
+      ?? these two functions will basically be the same, but the times color will be different. Would it make more sense to create one function, but pass in the team color? Would we be able to do that under one button? modify based on the class of the button? 
+
+    ge the id for each color
+    add that player to the associated color array
+    create an li for this item
+    remove the li from the list of players
+    add the extended class of Teammates to that person. 
+    display the person in the color group
+    update html content
+
+  exchange()
+  Lives in - 'blue' & 'red'
+    similar to the red and blue, 
+      if the player is currently colorA switch to colorB,
+        move that person to that color's team array. 
+        this person should not appear on the array or li of the previous team
+    update html content
+
+  edit()
+    Lives in - ? should live with each individual in every section? 
+      should appear from the point of them becoming players  and stay with the individual allowing updates to the player class per individual
+    update html content 
+
 
 Instructions Provided 
 Use the class keyword to create a template of a dodgeBallPlayer that requires canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience
--- 06week/dodgeBall.js
+++ 06week/dodgeBall.js
@@ -53,71 +53,127 @@ const arrOfPeople = [
   },
 ]
 
-//how many of these functions should actually be methods within the classes? 
-//what triggers these events? 
-//create a function by which we can merge (Object.assign(target, source)), the individual with the player class 
+//code the will be repeated a lot
+const cLi = document.createElement("li")
+const cBtn = document.createElement("button")
+//remember the name needs to be a 'string'
+const cClass = (element,name) => {element.classList.add(name)}
+const findQSA = (element,name) => { element.querySelectorAll(name)}
+const appending = (parent, child) => {parent.appendChild(child)}
+const dElement = (parent, child) => {parent.removeChild(child)}
 
-//create a function that allows individuals to be move from the list of player to the blue or red team
+const listPeopleChoices = () => {
+  const listElement = document.getElementById('people')
+  arrOfPeople.map(person => {
+    // console.log('this this map the whole object?',person) 
+    //yes, the whole object is mapped. 
+    const li = document.createElement("li")
+    const button = document.createElement("button")
+      console.log('button', button)
+    button.innerHTML = "Make Player"
+    button.addEventListener('click', function(){makePlayer(person.id)} )
+      li.appendChild(button)
+      li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
+      listElement.append(li)
+    })
+  }
 
-//create a function that allows people to be move from one team to another.
+  class player {
+    //these characteristic will have to be added to the individuals before they get added to the list of players
+    constructor(canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience){
+      //these would be the default answers on all individuals 
+      this.canThrowBall = canThrowBall;
+      this.canDodgeBall = canDodgeBall; 
+      this.hasPaid = hasPaid;
+      this.isHealthy = isHealthy;
+      this.yearsExperience = 0;
+    }
+    //add a method to update the player information 
+    updatePlayer() {
+      this.canThrowBall = this.canThrowBall;
+      this.canDodgeBall = this.canDodgeBall
+      this.hasPaid = this.hasPaid
+      this.isHealthy = this.isHealthy
+      this.yearsExperience = this.yearsExperience; 
+    }
+  }
 
-//create a function that allows the team attributes to be added to an individual once they have been added to that team.
-
+//team buttons? 
+const teamButton = (buttonName,colorFun) => {
+  buttonName.addEventListener('click',function() {
+    colorFun() 
+    console.log(`you clicked on ${buttonName}`)}) }
 
 //take the array of people and put it in this list w/ the player components added. 
 const listOfPlayers = []
+
+const makePlayer = (id) => {
+  // console.log(`li ${id} was clicked!`)
+  const dbPlayers = document.getElementById('player')
+  arrOfPeople.map(person => {
+    //pull in the whole object, but only add those individual to the listOfPlays, if their id was provided 
+    // console.log('look at the whole object,', person)
+    if(person.id === id) {
+      console.log('first if, inside makePlayer?')
+      //at this point, we want to create a new object which includes the player constructor and the person object from the arrayOfPeople
+          //we are assuming all of these when they sign up. 
+      let letsPlay = new player(false, false, false, false,0)
+      console.log(letsPlay)
+      let newPlayer = Object.assign(person, letsPlay)
+      console.log('merge to objects?',newPlayer)
+    listOfPlayers.push(newPlayer)
+    }
+  })
+//   console.log(listOfPlayers)
+
+//     let playerCLi
+//     let bBtn
+//     let rBtn 
+//   listOfPlayers.forEach(person => {
+//       playerCLi = cLi
+//     cClass(cLi, 'playerLi') //create Li for person
+//       playerCLi.innerText = player.name
+//      //create blue button
+//     bBtn = cBtn
+//     cClass(bBtn, 'blueBtn')
+//       bBtn.innerText = 'Blue'
+//       //create red button 
+//     rBtn = cBtn
+//     cClass(rBtn, 'redBtn')
+//       rBtn.innerText = 'Red'
+//   })
+
+//     // teamButton(bBtn, blue)
+//     // teamButton(rBtn, red)
+//     //   appending(playerCLi,bBtn)
+//     //   appending(playerCLi,rBtn)
+//     //   appending(dbPlayers,playerCLi)
+
+}
+
   //add people from your listOfPlayers to either the blueTeam or the RedTeam
 
 const blueTeam = []
   //if they have been moved to the blueTeam, remove them from the listOfPlayers 
       //add the team attributes to this player 
+const blue = () => {
+  console.log('blue day blue blaha')
+}
+
 const redTeam = []
   //of they have been moved to the redTeam, remove them from the listOfPlayers 
-
- 
-class player {
-  //these characteristic will have to be added to the individuals before they get added to the list of players
-  //possibly use Object.assign(target, source)
-  constructor(canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience){
-    //these would be the default answers on all individuals 
-    this.canThrowBall = False;
-    this.canDodgeBall = False; 
-    this.hasPaid = False;
-    this.isHealthy = False;
-    this.yearsExperience = 0;
-  }
-  //add a method to update the player information 
+const red = () => {
+  console.log('red rum')
 }
+ 
+
 
 //when people are added to their associated them, this class should be added to those individuals. 
  //should these be an extended class?
-class blueTeammate {
-  //these be supers? 
-  constructor(mascot, teamColor){
-    this.mascot = mascot,
-    this.teamColor = teamColor
-  }
-}
-class redTeammate {
-  constructor(mascot, teamColor){
-    this.mascot = mascot,
-    this.teamColor = teamColor
-  }
-}
-
-const listPeopleChoices = () => {
-  const listElement = document.getElementById('people')
-  arrOfPeople.map(person => {
-    const li = document.createElement("li")
-    const button = document.createElement("button")
-    button.innerHTML = "Make Player"
-    button.addEventListener('click', function() {makePlayer(person.id)} )
-    li.appendChild(button)
-    li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
-    listElement.append(li)
-  })
-}
-
-const makePlayer = (id) => {
-  console.log(`li ${id} was clicked!`)
-}
+// class teammates extends players  {
+//   constructor(){
+//     super(canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience, mascot, teamColor)
+//     this.mascot = mascot,
+//     this.teamColor = teamColor
+//   }
+// }
