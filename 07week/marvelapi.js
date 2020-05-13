9915e56a92e8f49d2f5a34887f969de88e06e105
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2020-05-12 22:03:46 -0500

    simplified the concept.

--- 07week/marvelapi.js
+++ 07week/marvelapi.js
@@ -21,38 +21,40 @@ let getHeroes = () => {
     return result
   })
   .then(function(result) {
+      console.log('result',result)
   //should return each item in the array as individual to loop through 
   result.forEach(needAHero)
 })
 }
 
-//tracks each item created so it doesn't replace the individual and actually creates the new element 
-let usedId = []
-
-let needAHero = (characters) => {
+let needAHero = (character) => {
   //create a list of all the comic stories 
-  //console.log('my results being pulled in?',characters) //yes
+  // console.log('my results being pulled in?',character) //yes
+  // console.log(Object.keys(character))
   //the characters are already is alphabetical order
   let main = document.getElementsByTagName('main') 
   let div = document.getElementById('character')
 
-  if (usedId.includes(characters.id) === false) {
+
   let charCard = document.createElement('div')
     charCard.setAttribute('id', 'charCard')
 
   let charUL = document.createElement('ul')
     charUL.setAttribute('id', 'charUL')
 
-  let pic = characters.thumbnail
+  let pic = character.thumbnail.path + "/portrait_xlarge." +character.thumbnail.extension
   // console.log('picture path',pic)
-  let name = characters.name
+  let name = character.name
   // console.log('name', name)
   let profilePic  = document.createElement('img')
   let heroName = document.createElement('li')
   let storyBtn = document.createElement('button')
-    storyBtn.innerText = 'Their Stories'
     storyBtn.setAttribute('id', 'storyButton')
     storyBtn.setAttribute('aria-controls','forToggle')
+    storyBtn.innerText = 'Their story'
+  let hiredBtn = document.createElement('button')
+  hiredBtn.setAttribute('id', 'hiredButton')
+    hiredBtn.innerText = 'HIRE'
 
   profilePic.src = pic
   heroName.innerText = name
@@ -61,96 +63,44 @@ let needAHero = (characters) => {
   charUL.appendChild(profilePic)
   charUL.appendChild(heroName)
   charUL.appendChild(storyBtn)
+  charUL.appendChild(hiredBtn)
 
   let storyUL = document.createElement('ul')
-  
-  let stories = characters.stories.items
-  // console.log(stories)
-  let listOfStories = []
+  storyUL.setAttribute('id', 'storyTime')
+  storyUL.setAttribute('class', 'forToggle')
 
-//go through each character and pull out the items listed in the stories key and add them to their own list
-  stories.forEach((element) => {
-    listOfStories.push(element.name)
-  });
+  const dis = character.description
+  // console.log('how long a story?',dis.length)
+  const liLis = document.createElement('li')
 
-  //discovered there may be duplicates in the list of stories
-  let shortList = []
-  let removeDuplicates = (array) => {
-    console.log('pulling in',array)
-
-    let dup = []
-    for (let i = 0; i < array.length; i++) {
-      for(let j = 1; i < array.length; j++) {
-        if(array[i] === (array[j])) {
-          dup.push(array[i])
-        } else {
-          shortList.push(array[i])
-        }
-        shortList
-      }
-      return shortList
-    }
-    console.log('clean', shortList)
-    console.log('dups', dup)
-    return shortList
+  if (dis.length === 0) {
+    liLis.innerText = `${name}'s story has yet to be told.`
+  } else {
+    liLis.innerText = dis
   }
-
-  removeDuplicates(listOfStories)
-  // console.log('before merge',listOfStories)
-
-  // //should sort the list before publishing in the webpage 
-  // const mergeSort = (array) => {
-  //   // console.log('inside merge sorts array:', array )
-  //   if(array.length <= 1) {
-  //     return array
-  //   }
-  //   const middle = Math.floor(array.length/2)
-  //   const left = array.slice(0, middle)
-  //   const right = array.slice(middle)
-
-  //   let merge = (left, right) => {
-  //     let resultArray = [] 
-  //     let leftIndex = 0
-  //     let rightIndex = 0;
-
-  //   while (leftIndex < left.length && rightIndex < right.length) {
-  //   if (left[leftIndex] < right[rightIndex]) {
-  //     resultArray.push(left[leftIndex]);
-  //     leftIndex++;
-  //   } else {
-  //     resultArray.push(right[rightIndex]);
-  //     rightIndex++; 
-  //   }
-  // }
-  //   return resultArray
-  //         .concat(left.slice(leftIndex))
-  //         .concat(right.slice(rightIndex));
-  //   }
-  //   return merge(
-  //     mergeSort(left), mergeSort(right)
-  //   )
-  // }
-
-  // mergeSort(listOfStories)
-
-  // console.log('after merge',listOfStories)
-
-  // let tales = listOfStories.forEach((element, index) => {
-  //   // console.log('title }', element, 'index of :', index)
-  //   let liTitle = 'createLi' + index
-  //   liTitle = document.createElement('li')
-  //   liTitle.innerText = element
-  //   liTitle.setAttribute('id', 'tales')
-  // })
-
-  // console.log(tales)
-  // storyUL.appendChild(tales)
-    //sort that list alphabetical order under the name of each character using a button
+  // console.log('tell me a story:',dis)
   
-  //display the names of each character that appears in that comic. 
+  storyUL.appendChild(liLis)
+  
+
+  storyBtn.addEventListener('click', () => {
+    console.log('storytime for the hero')
+
+    let aria = storyUL.getAttribute('aria-hidden')
+    storyUL.setAttribute('aria-hidden', aria === 'true' ? 'false' : 'true')
+
+    if (aria === 'true') {
+      storyUL.style.visibility = 'visible' 
+      storyBtn.innerText = 'Their story'
+
+    } else {
+      storyUL.style.visibility = 'hidden' 
+      storyBtn.innerText = 'Story is over'
+    }
+    charUL.appendChild(storyUL)
+  })
+
   div.appendChild(charUL)
 
 } 
-usedId.push(character.id)
-}
-// console.log(usedId)
+// }
